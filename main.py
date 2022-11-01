da942baffa3b0eb6d3801b5c0ae855c906050609
psergicv <sv.postolachi@gmail.com>
2022-11-01 20:23:48 +0200

    edit and delete function added

--- main.py
+++ main.py
@@ -36,19 +36,34 @@ def create():
 @app.route('/<int:post_id>')
 def post(post_id):
     conn = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    conn.execute("SELECT title, message FROM posts WHERE id = %s", (post_id,))
+    conn.execute("SELECT id, title, message FROM posts WHERE id = %s", (post_id,))
     detailed_post = conn.fetchone()
     return render_template("detailed_post.html", detailed_post=detailed_post)
 
 
 @app.route('/<int:post_id>/edit', methods=['GET', 'POST'])
 def edit(post_id):
-    pass
+    conn = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    conn.execute("SELECT title, message FROM posts WHERE id = %s", (post_id,))
+    old_data = conn.fetchone()
+
+    if request.method == "POST":
+        title = request.form['title']
+        message = request.form['message']
+        conn.execute("UPDATE posts SET title = %s, message = %s", (title, message))
+        conn.connection.commit()
+        conn.close()
+        return redirect(url_for('index'))
+
+    return render_template("edit.html", old_data=old_data)
 
 
 @app.route('/<int:post_id>/delete')
 def delete(post_id):
-    pass
+    conn = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    conn.execute("DELETE FROM posts WHERE id = %s", (post_id,))
+    conn.connection.commit()
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
